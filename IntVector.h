a40ce154b431c1bbc47c4fb208f42f2e533d8385
neyron8 <32508305+neyron8@users.noreply.github.com>
2018-03-11 20:53:04 +0700

    I dont know why it is here

--- IntVector.h
+++ IntVector.h
@@ -1,8 +1,16 @@
-#include <stdio.h>
+#ifndef INTVECTOR_C
+#define INTVECTOR_C
 
 typedef struct {
 	size_t *ptr;
-    size_t size;
-    size_t used;
-} darray;
+        size_t size;
+        size_t used;
+} IntVector ;
 
+IntVector *int_vector_new ( size_t initial_capacity );
+IntVector *int_vector_copy ( const IntVector *v );
+void int_vector_free ( IntVector *v );
+size_t int_vector_get_size ( const IntVector *v );
+size_t int_vector_get_capacity ( const IntVector *v );
+
+#endif
