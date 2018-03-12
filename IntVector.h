c9b37f8c7a6650b9c5e4744588d915262ef8b207
Николай <dark.lords@mail.ru>
2018-03-12 16:51:59 +0700

    winda

--- IntVector.h
+++ IntVector.h
@@ -13,5 +13,6 @@ IntVector *int_vector_copy ( const IntVector *v );
 void int_vector_free ( IntVector *v );
 size_t int_vector_get_size ( const IntVector *v );
 size_t int_vector_get_capacity ( const IntVector *v );
+int int_vector_shrink_to_fit(IntVector *v);
 
 #endif
