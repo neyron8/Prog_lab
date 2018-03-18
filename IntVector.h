6ac3e36fb3c97a0589f0d592d8548e12977a5945
neyron8 <32508305+neyron8@users.noreply.github.com>
2018-03-18 09:25:29 +0700

    Update IntVector.h

--- IntVector.h
+++ IntVector.h
@@ -9,7 +9,7 @@ typedef struct {
 
 IntVector *int_vector_new ( size_t initial_capacity );
 IntVector *int_vector_copy ( const IntVector *v );
-void int_vector_print ( IntVector *v , char namesrc [] );
+void int_vector_print ( IntVector *v );
 void int_vector_free ( IntVector **v );
 int int_vector_get_item ( const IntVector *v, size_t index );
 void int_vector_set_item ( IntVector *v, size_t index, int item );
